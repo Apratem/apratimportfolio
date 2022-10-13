a290dcc4a517e16b5558b1256e1dbddb0735981e
Apratim Prajapati <112246317+apratemprajapati@users.noreply.github.com>
2022-10-13 20:24:50 +0545

    Portfolio V1.2
    added dark mode light mode color changer typing animation
    october 13 2022

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*=== typing animation === */
+var typed = new Typed(".typing",{
+    strings:["","Graphic Designer","Biotechnologist","Student"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
